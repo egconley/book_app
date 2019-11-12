6548dce91e3236b8998562154445c039b065f594
katerynashydlovska <shydlovskayaka@gmail.com>
2019-11-12 10:56:00 -0800

    /

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const superagent = require('superagent');
 const app = express();
 const PORT = process.envPORT || 3000;
 
@@ -13,6 +14,8 @@ app.get('/', (request, response) => {
   response.render('index');
 })
 
+app.post('/serches', searchHendler);
+
 function Book(items) {
   // const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
   this.id = items.id;
@@ -23,8 +26,20 @@ function Book(items) {
   this.image = `https://books.google.com/books/content?id=${items.id}&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api`;
   // link grabbed from items.volumeInfo.imageLinks.thumbnail property.
 }
-}
 
+function searchHendler(request, response) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  console.log(request.body);
+  console.log(request.body.search);
+
+  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', { searchResults: results }));
+}
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 ///
