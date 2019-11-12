6f8700c2da695ea20ae999d9551293b4e5996e22
egconley <31393715+egconley@users.noreply.github.com>
2019-11-12 10:18:32 -0800

    deleted duplicate constructor

--- server.js
+++ server.js
@@ -20,21 +20,10 @@ function Book(items) {
   this.author = items.volumeInfo.info.authors || 'No author available';
   this.description = items.volumeInfo.description || 'No description available';
   // this.image = items.volumeInfo.imageLinks.thumbnail;
-  this.image = `https://books.google.com/books/content?id=${items.id}&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api`;
+  this.image = `https://books.google.com/books/content?id=${items.id}&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api`; // link grabbed from items.volumeInfo.imageLinks.thumbnail property.
 }
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-///
-
-function Book(items) {
-  // const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
-  this.id = items.id;
-  this.title = items.volumeInfo.title || 'No title available';
-  this.author = items.volumeInfo.info.authors || 'No author available';
-  this.description = items.volumeInfo.description || 'No description available';
-  // this.image = items.volumeInfo.imageLinks.thumbnail;
-  this.image = `https://books.google.com/books/content?id=${items.id}&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api`; // link grabbed from items.volumeInfo.imageLinks.thumbnail property.
-}
 
 
