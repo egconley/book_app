e56e0cce0fe2540541f7cbd901239994ba4fcfb5
egconley <31393715+egconley@users.noreply.github.com>
2019-11-12 09:29:38 -0800

    added view engine and get request for index.ejs

--- server.js
+++ server.js
@@ -7,5 +7,10 @@ const PORT = process.envPORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+  response.render('index');
+})
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
